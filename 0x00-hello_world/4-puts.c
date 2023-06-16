d56374097491fbdd5c98873f88afc0d2d1cff473
zedo <zerihun699@gmail.com>
2023-06-16 02:25:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts "Programming is like building a multilingual puzzle./n"
+#include <studio.h>
+/**
+ *main - Entry point
+ *Return : always 0 for success
+*/
+int main(void)
+{
+   puts("\"Programming is like building a multilingual puzzle\")
+   return (0);
+}
