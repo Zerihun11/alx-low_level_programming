5416bca8d8fb0392617ccf12717a541ca742f8ba
zedo <zerihun699@gmail.com>
2023-06-16 02:48:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include<studio.h>
 /**
  *main - Entry point
- *Return : always 0 (Success)
+ *Return : always 0 for (Success)
 */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle\");
+   puts("\"Programming is like building a multilingual puzzle\n");
    return (0);
 }
