189327777c48645bcc7626e5a686615f66dca8de
zedo <zerihun699@gmail.com>
2023-06-24 02:47:59 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_prodacts of twe num
+ * @mul  to print.
+ *
+ *
+ */
+    int mul(int a, int)
+{
+        return a * b;
+}
+void main()
+{
+        int *function_pointer;
+        function_pointer = mul;
+        printf("The product of two numbers is:%d",mul);
+}
