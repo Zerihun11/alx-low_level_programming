e6c21a580aa11b0390f0aa7804ce2683ac73235e
zedo <zerihun699@gmail.com>
2023-06-25 12:34:27 -0700

    0-pec

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * main - Entry points _putchar and a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
          _putchar('_');
          _putchar('p');
@@ -18,6 +15,7 @@ int main(void)
          _putchar('a');
          _putchar('r');
          _putchar('\n');
-         return(0);
+         
+	 return (0);
 
 }
