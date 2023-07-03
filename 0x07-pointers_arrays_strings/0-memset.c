db9e09a23326c7a47d6cbad7a3bc5c37a8cc5b0b
zedo <zerihun699@gmail.com>
2023-07-03 08:16:37 -0700

    alx-low_level_programming

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * _memset - this funtion fills memory with a constant byte
- * @s: Starting addres of memory
- * @b: the desired values
- * @n: number of bytes to the change
+ * _memset - this funtion fills memory with a constant byte.
+ * @b: a constant byte.
+ * @n: number of bytes.
+ * @s: pointers to memory.
  *
- * Return: Changed arrey
+ * Return: A pointer to memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
