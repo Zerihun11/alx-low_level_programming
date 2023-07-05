3366bf3b28d8c055d1da994bb02b6742ff859f6c
zedo <zerihun699@gmail.com>
2023-07-05 05:35:54 -0700

    0x08-recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ * Return: Always Succes 1 or faile(-1)
+ */
+int _strlen_recursion(char *s)
+{
+	if (*)
+	{
+		return (1 + _strlen_recursion((s + 1)));
+		return (0);
+	}
+}
