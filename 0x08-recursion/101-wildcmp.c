fea1c0f72c969bf68df08936910ed4f38cc6575d
zedo <zerihun699@gmail.com>
2023-07-06 14:10:30 -0700

    100-wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * wildcmp - Converting fanction
+ * @s1: input string
+ * @s2: input string
+ * Return: Always Succes (0)
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int result;
+
+	ressult = wildcmp(*s1, *s2);
+	if (*s1 == *s2)
+		return (1);
+	else
+		return (0);
+}
+
