dee4cecff1789d3de60d666984064a7b016013f5
Zerihun11 <zerihun699@gmail.com>
2023-08-05 00:37:17 -0700

    bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,22 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num, i;
+	int len;
 
-	if (!b || !*b)
+	if (b == NULL)
 		return (0);
 
-	num = i = 0;
-	while (b[i])
+	for (len = 0; b[len]; len++)
 	{
-		if (b[i] > 49)
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		else if (b[i] == 49)
-		{
-			num <<= 1;
-			num += 1;
-		}
-		else
-			num <<= 1;
-		i++;
 	}
+
+	for (i = 1, num = 0, len--; len >= 0; len--, i *= 2)
+	{
+		if (b[len] == '1')
+			num += i;
+	}
+
 	return (num);
 }
