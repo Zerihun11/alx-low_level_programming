ba18eaa73bef097e4426e1a1ecde9d6083317c00
Zerihun11 <zerihun699@gmail.com>
2023-08-31 10:31:09 -0700

    1-print

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@ void print_binary(unsigned long int n)
 	int i, result = 0;
 	unsigned long int values;
 
-	for (i <= x; x > 0; i++)
+	for (i = 63; i >= 0; i--)
 	{
 		values = n >> i;
 		if (values & 1)
