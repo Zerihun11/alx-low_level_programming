71f26f46aa9083ef475138cb09c6e8e8da01fa8d
Zerihun11 <zerihun699@gmail.com>
2023-08-31 05:05:16 -0700

    bit_munipulation

---
+++ 0x14-bit_manipulation/0.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/**
+ * binary_to_uint - convert binary number
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable charr
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int len = strlen(b);
+
+	for (int i = 0; i < len; i++)
+	{
+		if (b[i] == '1')
+		{
+			result += pow(2, len - i - 1);
+		}
+		else if (b[i] != '0')
+			return (0);
+	}
+
+	return (result);
+}
