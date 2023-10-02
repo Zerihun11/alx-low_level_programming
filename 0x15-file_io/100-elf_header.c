e1e6a28694a9ac49993a7ea30e1236f3c4111fea
root <Zerihun11>
2023-10-02 12:27:19 -0700

    file I/O

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,11 +10,11 @@ void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
+//void print_abi(unsigned char *e_ident);
+//void print_osabi(unsigned char *e_ident);
+//void print_type(unsigned int e_type, unsigned char *e_ident);
+//oid print_entry(unsigned long int e_entry, unsigned char *e_ident);
+//void close_elf(int elf);
 
 /**
  * check_elf - Checks if a file is an ELF file.
